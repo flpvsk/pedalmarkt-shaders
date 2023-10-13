950f63769ca2336ed4a7581eddbc0a1302538356
flpvsk <filipovskii.off@gmail.com>
2023-10-13 17:55:27 +0200

    Python run script works

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ```bash
-glslViewer src/textwrap.frag  -u_fullOfTextTexture assets/full-of-text.png  -w 400 -h 300
-```
-
-```bash
-glslViewer src/main.frag  -u_perryMonochrome assets/perry-monochrome.png -u_paperTexture assets/copyscan14.jpg  -w $((4 * 100)) -h $((3 * 100))
+python3 run.py
 ```
--- run.py
+++ run.py
@@ -2,24 +2,45 @@ import asyncio
 import subprocess
 
 async def main():
-  print('start')
   cmd = [
     'glslViewer',
     'src/main.frag',
-    '-u_perryMonochrome assets/perry-monochrome.png',
-    '-u_paperTexture assets/copyscan14.jpg',
+    '-u_perryMonochrome', 'assets/perry-monochrome.png',
+    '-u_paperTexture', 'assets/copyscan14.jpg',
+    '-fps', '12',
+    '-w', '400', '-h', '300',
     '-noncurses',
-    '-w 100 -h 100',
   ]
   proc = subprocess.Popen(
     cmd,
     stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
     encoding='utf8',
-    shell=True
   )
-  await asyncio.sleep(4)
-  proc.communicate(input='defines,SCENE_2\n')
-  await asyncio.sleep(1000)
+
+  await asyncio.sleep(1)
+  scene = 0
+  scenes = 4
+
+  while (True):
+    prev_scene = (scene - 1) % scenes
+    print(
+      'Scene {}, prev scene {}'.format(
+        scene + 1, prev_scene +1
+      )
+    )
+
+    if (proc.stdin == None):
+      return
+
+    proc.stdin.write(
+      'defines,SCENE_{}\nundefine,SCENE_{}\n'.format(
+        scene + 1,
+        prev_scene + 1
+      )
+    )
+    proc.stdin.flush();
+
+    scene = (scene + 1) % scenes
+    await asyncio.sleep(20)
 
 asyncio.run(main())
