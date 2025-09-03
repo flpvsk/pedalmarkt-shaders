8de36105aafb120b86d34711926b6e84d11dea95
flpvsk <filipovskii.off@gmail.com>
2025-09-03 12:41:53 +0200

    Reformat

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
+Code for the TV in the display window of Pedal Markt.
+
+## Requirements
+
+* [glslViewer](https://github.com/patriciogonzalezvivo/glslViewer)
+* [python3](https://www.python.org/downloads/)
+* [Folder of assets](https://drive.google.com/drive/folders/1lgjRLbbvZuYJF8BKc0wfflkvU1bMVKeY?usp=sharing)
+
+## Running locally
+
+1. Download the `assets` folder and place into the root of
+   the project;
+
+2. Run
+
+    ```bash
+    python3 run.py
+    ```
+
+3. To see different options available in the script, run
+
+    ```bash
+    python3 run.py -h
+    ```
+
+## Adding a scene
+
+Scenes in the project are written as fragment shaders (aka
+pixel shaders). To add a scene:
+
+1. Add a shader function in a separate file, see
+   `src/march.glsl` as an example. Use a unique function
+   name;
+
+2. Include the new `.glsl` file into `src/main.glsl`;
+
+3. Increment the scene number, add an `#ifdef` condition in
+   the main function for the variable `SCENE_X`
+
+4. Add the scene name to the `scene_names` array in
+   `./run.py`
+
+## Formatting python code
+
 ```bash
-python3 run.py
+python3 -m pyink --pyink-indentation 2 run.py
 ```
+
+
+
--- run.py
+++ run.py
@@ -4,103 +4,147 @@ import os
 import random
 import argparse
 
+scene_names = [
+    "scope",
+    "rules",
+    "shape",
+    "comet",
+    "march",
+    "text",
+]
+
+
 async def main():
-  parser = argparse.ArgumentParser(add_help=False)
+  parser = argparse.ArgumentParser()
   parser.add_argument(
-    '-f',
-    '--fullscreen',
-    type=bool,
-    default=False
+      "-f",
+      "--fullscreen",
+      type=bool,
+      action=argparse.BooleanOptionalAction,
+      default=False,
+      help="fullscreen mode, window size arguments will be ignored (default: %(default)s)",
   )
   parser.add_argument(
-    '-a',
-    '--audio',
-    type=bool,
-    default=False
+      "-a",
+      "--audio",
+      type=bool,
+      action=argparse.BooleanOptionalAction,
+      default=False,
+      help='enable audio input and add audio-reactive "scope" scene (default: %(default)s)',
   )
-  parser.add_argument('-w', '--width', type=int, default=400)
-  parser.add_argument('-h', '--height', type=int, default=300)
+  parser.add_argument(
+      "-ww",
+      "--width",
+      type=int,
+      default=400,
+      help="window width (default: %(default)s)",
+  )
+  parser.add_argument(
+      "-wh",
+      "--height",
+      type=int,
+      default=300,
+      help="window height (default: %(default)s)",
+  )
+  parser.add_argument(
+      "-s",
+      "--scene",
+      choices=list(range(len(scene_names))) + scene_names,
+      help="play only one of the scenes, accepts an index or a scene name",
+  )
   args = parser.parse_args()
+  scene_idx = None
+
+  if args.scene.isdecimal():
+    scene_idx = int(args.scene.idx)
+    if scene_idx < 0 and scene_idx >= len(scene_names):
+      scene_idx = None
+
+  if args.scene and scene_idx == None:
+    for i in range(len(scene_names)):
+      if scene_names[i] == args.scene:
+        scene_idx = i
+        break
 
   cmd = [
-    'glslViewer',
-    'src/main.frag',
-    '-u_perryMonochrome', 'assets/perry-monochrome.png',
-    '-u_paperTexture', 'assets/copyscan14.jpg',
-    '-u_textTexture', 'assets/pedal-expo-text.png',
-    '-fps', '12',
-    '--noncurses',
-    '--nocursor',
-    # '-f',
-    # '-w', '400', '-h', '300',
-    # '--audio',
+      "glslViewer",
+      "src/main.frag",
+      "-u_perryMonochrome",
+      "assets/perry-monochrome.png",
+      "-u_paperTexture",
+      "assets/paper.jpg",
+      "-u_textTexture",
+      "assets/pedal-expo-text.png",
+      "-fps",
+      "12",
+      "--noncurses",
+      "--nocursor",
+      # '-f',
+      # '-w', '400', '-h', '300',
+      # '--audio',
   ]
 
   if args.fullscreen:
-    cmd.append('-f')
+    cmd.append("-f")
 
   if args.audio:
-    cmd.append('-a')
+    cmd.append("-a")
 
-  if (
-    not args.fullscreen
-    and args.width > 0
-    and args.height > 0
-  ):
-    cmd.append('-w')
+  if not args.fullscreen and args.width > 0 and args.height > 0:
+    cmd.append("-w")
     cmd.append(str(args.width))
-    cmd.append('-h')
+    cmd.append("-h")
     cmd.append(str(args.height))
 
   print(cmd)
 
   proc = subprocess.Popen(
-    cmd,
-    stdin=subprocess.PIPE,
-    encoding='utf8',
-    env={
-      'DISPLAY': ':0.0',
-      'PATH': os.environ['PATH'],
-    },
+      cmd,
+      stdin=subprocess.PIPE,
+      encoding="utf8",
+      env={
+          "DISPLAY": ":0.0",
+          "PATH": os.environ["PATH"],
+      },
   )
 
   await asyncio.sleep(1)
   scope_scene = 0
   text_scene = 5
-  other_scenes = [ 1, 2, 3, 4]
+  skip_text_scene = False
+  other_scenes = [1, 2, 3, 4]
   prev_scene = 6
   text_scene_probability = 0.3
+
+  if scene_idx != None:
+    skip_text_scene = True
+    other_scenes = [scene_idx]
 
   if args.audio:
     other_scenes.append(scope_scene)
 
-  while (True):
-    if (proc.stdin == None):
+  while True:
+    if proc.stdin == None:
       return
 
-    if random.random() < text_scene_probability:
+    if random.random() < text_scene_probability and not skip_text_scene:
       prev_scene = show_scene(proc, text_scene, prev_scene)
       await asyncio.sleep(12)
 
-    prev_scene = show_scene(
-        proc,
-        random.choice(other_scenes),
-        prev_scene
-    )
+    prev_scene = show_scene(proc, random.choice(other_scenes), prev_scene)
 
     await asyncio.sleep(32)
 
-def show_scene(proc, scene, prev_scene):
-    if scene == prev_scene:
-      prev_scene = 100
 
-    proc.stdin.write(
-      'defines,SCENE_{}\nundefine,SCENE_{}\n'.format(
-        scene + 1,
-        prev_scene + 1
-      )
-    )
-    proc.stdin.flush();
-    return scene
+def show_scene(proc, scene, prev_scene):
+  if scene == prev_scene:
+    prev_scene = 100
+
+  proc.stdin.write(
+      "defines,SCENE_{}\nundefine,SCENE_{}\n".format(scene + 1, prev_scene + 1)
+  )
+  proc.stdin.flush()
+  return scene
+
 
 asyncio.run(main())
